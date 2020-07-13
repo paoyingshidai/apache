1991d11a237be1fe0d4a559d33d9fe8c601415a4
Michael <13432849094@163.com>
2020-07-13 19:47:30 +0800

    添加 curator watcher 例子

--- zookeeper/src/test/java/com/michael/apache/zookeeper/CreateClientExamples.java
+++ zookeeper/src/test/java/com/michael/apache/zookeeper/CreateClientExamples.java
@@ -1,16 +1,87 @@
 package com.michael.apache.zookeeper;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
+import org.apache.curator.framework.recipes.cache.CuratorCacheBridge;
+import org.apache.curator.framework.recipes.cache.CuratorCacheBridgeBuilder;
+import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 import org.junit.Test;
 
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * https://github.com/apache/curator/blob/master/curator-examples/src/main/java/framework/CreateClientExamples.java
  */
+@Slf4j
 public class CreateClientExamples {
+
+
+    @Test
+    public void testWatcher() throws Exception {
+        ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = createWithOptions("localhost:2181", retryPolicy, 100, 10000);
+        client.start();
+
+        client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath("/michael", "Michael".getBytes());
+
+        final CuratorCache cache = CuratorCache.bridgeBuilder(client, "/michael").build();
+        cache.start();
+        cache.listenable().addListener((type, oldData, data) -> {
+
+            System.out.println(type);
+            System.out.println(new String(oldData.getData()));
+            System.out.println(new String(data.getData()));
+        });
+
+        new CountDownLatch(1).await();
+
+        client.close();
+    }
+
+
+    /**
+     * 路径监听
+     * @throws Exception
+     */
+    @Test
+    public void testChildWatcher() throws Exception {
+        ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = createWithOptions("localhost:2181", retryPolicy, 100, 10000);
+        client.start();
+
+        client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath("/child", "Michael".getBytes());
+
+        final CuratorCache cache = CuratorCache.bridgeBuilder(client, "/child").build();
+        cache.start();
+        cache.listenable().addListener((type, oldData, data) -> {
+
+            System.out.println(type);
+            System.out.println(new String(oldData.getData()));
+            System.out.println(new String(data.getData()));
+        });
+
+        new CountDownLatch(1).await();
+
+        client.close();
+    }
+
 
 
     @Test
@@ -23,6 +94,9 @@ public class CreateClientExamples {
                 .creatingParentsIfNeeded()
                 .withMode(CreateMode.EPHEMERAL)
                 .forPath("/michael", "Michael".getBytes());
+
+        byte[] bytes = client.getData().forPath("/michael");
+        String s = new String(bytes);
 
         client.close();
     }
@@ -39,7 +113,6 @@ public class CreateClientExamples {
                 .retryPolicy(retryPolicy)
                 .connectionTimeoutMs(connectionTimeoutMs)
                 .sessionTimeoutMs(sessionTimeoutMs)
-                .namespace("michael")
                 .build();
     }
 
